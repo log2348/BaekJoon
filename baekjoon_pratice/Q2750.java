dd78ab83291837b76fc5da59b8cdc3a4fa0e51d0
DESKTOP-6THI93T\jh234 <pjh4794@gmail.com>
2022-03-17 11:25:15 +0900

    정렬_수정렬하기

--- baekjoon_pratice/Q2750.java
+++ baekjoon_pratice/Q2750.java
@@ -7,12 +7,11 @@ public class Q2750 {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("정렬할 숫자의 개수: ");
+		// System.out.print("정렬할 숫자의 개수: ");
 		int n = scanner.nextInt();
 		
 		int[] numArray = new int[n];
 		
-		System.out.print("정렬할 숫자: ");
 		for(int i = 0; i < n; i++) {
 			numArray[i] = scanner.nextInt();
 		}
